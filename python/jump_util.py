7892a354c830bf3d7774a08ae11366ca71bd2d17
Vaclav Barta <vbarta@mangrove.cz>
2018-11-10 21:17:21 +0100

    moved lower part of jump_util into standalone module

--- python/jump_util.py
+++ python/jump_util.py
@@ -1,27 +1,13 @@
 from urllib.parse import quote
 import re
 from common import space_rx
+from rulebook import CityLevel, councillor_position_entities, deputy_mayor_position_entity, get_org_name, mayor_position_entities, minister_position_entity, rule_book
 
 # we could include single quote, but there probably aren't any Czech
 # politicians named O'Something...
 name_char_rx = re.compile("[^\\w ./-]")
 
 city_start_rx = re.compile("^(?:mč|město|městská část|obec|statutární město) ")
-
-university_name_rx = re.compile("\\b(?:univerzita|učení)")
-
-# ministers are special because their position is a subclass of
-# minister - normally minister of <resort> of Czech Republic
-minister_position_entity = 'Q83307'
-
-# mayors are special because there's so many the name match may
-# produce false positives - so for mayors we also match the city
-mayor_position_entities = ( 'Q30185', 'Q147733' )
-
-deputy_mayor_position_entity = 'Q581817'
-
-# councillors are even commoner than mayors
-councillor_position_entities = ( 'Q708492', 'Q19602879', 'Q4657217' )
 
 def normalize_name(name):
     return name_char_rx.sub("", name.strip())
@@ -38,90 +24,73 @@ def normalize_city(name):
     shorter = city_start_rx.sub("", safer)
     return shorter.strip()
 
-def make_position_set(detail):
-    university2rector = {
-        'Univerzita Karlova': 'Q12049166',
-        'Univerzita Karlova v Praze': 'Q12049166'
-    }
+def convert_answer_to_iterable(answer, it):
+    if callable(answer): # technically we could have a cycle, but hopefully nobody will need that...
+        answer = answer(it)
 
+    if isinstance(answer, str):
+        return (answer,)
+    else: # must be iterable
+        return answer
+
+def make_position_set(detail):
     sought = set()
     lst = detail['workingPositions']
     for it in lst:
-        org_name = it['organization'].strip()
+        org_name = get_org_name(it)
         if org_name == 'Nejvyšší státní zastupitelství':
             sought.add('Q26197430')
 
         wp = it['workingPosition']
-        if wp['name'] == 'člen řídícího orgánu':
-            rector = university2rector.get(org_name)
-            if rector:
-                sought.add(rector)
-
-            if rector or university_name_rx.search(org_name.lower()):
-                sought.add('Q212071')
-                sought.add('Q2113250')
-                sought.add('Q723682')
-
-        if wp['name'] == 'vedoucí zaměstnanec 3. stupně řízení':
-            if (org_name == 'Kancelář prezidenta republiky'):
-                sought.add('Q15712674')
-            else:
-                sought.add('Q1162163')
-
-        if wp['name'] == 'člen vlády':
-            # currently matching only ministers, but 'člen vlády' is
-            # also used for (at least some) deputy ministers...
-            sought.add(minister_position_entity)
-        elif wp['name'] == 'náměstek pro řízení sekce':
-            sought.add('Q15735113')
-        elif wp['name'] == 'starosta':
-            for pos in mayor_position_entities:
+        wp_name = wp['name']
+        answer = rule_book.get(wp_name)
+        if answer:
+            answer = convert_answer_to_iterable(answer, it)
+            for pos in answer:
                 sought.add(pos)
-        elif wp['name'] == 'místostarosta / zástupce starosty':
-            sought.add(deputy_mayor_position_entity)
-        elif wp['name'] in ( 'člen zastupitelstva', 'člen Rady' ):
-            for pos in councillor_position_entities:
-                sought.add(pos)
-        elif wp['name'] == 'člen bankovní rady České národní banky': # missing the governor
-            sought.add('Q28598459')
-        elif (wp['name'] == 'soudce'):
-            sought.add('Q16533')
-        elif (wp['name'] == 'ředitel bezpečnostního sboru'):
-            sought.add('Q1162163')
-        elif wp['name'] == 'vedoucí zastupitelského úřadu':
-            sought.add('Q121998')
 
-        if (wp['name'] == 'poslanec') or wp['deputy']:
+        # probably synonymous, and could be included in rule_book, but
+        # just to play it safe...
+        if (wp_name == 'poslanec') or wp['deputy']:
             sought.add('Q1055894')
             sought.add('Q19803234') # should be a subset but isn't
             sought.add('Q486839')
-
-        if (wp['name'] == 'senátor') or wp['senator']:
+        elif (wp_name == 'senátor') or wp['senator']:
             sought.add('Q15686806')
             sought.add('Q18941264')
             sought.add('Q486839')
 
     return sought
 
-def make_city_set_for_mayor(detail):
+def make_city_set(detail, representative):
     sought = set()
     lst = detail['workingPositions']
     for it in lst:
         wp = it['workingPosition']
-        if wp['name'] == 'starosta':
-            sought.add(normalize_city(it['organization']))
+        answer = rule_book.get(wp['name'])
+        if answer is not None and isinstance(answer, CityLevel):
+            answer = convert_answer_to_iterable(answer, it)
+            if representative in answer:
+                sought.add(normalize_city(it['organization']))
 
     return sought
 
-def make_city_set_for_municipal(detail):
-    sought = set()
-    lst = detail['workingPositions']
-    for it in lst:
-        wp = it['workingPosition']
-        if wp['name'] in ( 'místostarosta / zástupce starosty', 'člen zastupitelstva', 'člen Rady' ):
-            sought.add(normalize_city(it['organization']))
+def format_councillor_bare_clause(detail, councillor_position_iterable):
+    first = next(iter(councillor_position_iterable))
+    city_set = make_city_set(detail, first)
+    if not len(city_set):
+        return None
 
-    return sought
+    vl = ' '.join('wd:' + p for p in sorted(councillor_position_iterable))
+
+    # equality should be sufficient but actually doesn't match some labels
+    filter_expr = ' || '.join('strstarts(lcase(?t), "%s")' % c for c in sorted(city_set))
+
+    return """values ?p { %s }
+        ?w p:P39/pq:P642 ?c.
+        ?c rdfs:label ?t.
+        filter(lang(?t) = "cs").
+        filter(%s).""" % (vl, filter_expr)
 
 def make_query_url(detail, position_set):
     city2mayor = {
@@ -140,24 +109,27 @@ def make_query_url(detail, position_set):
 
     position_list = list(position_set)
 
+    minister_position = None
+    if minister_position_entity in position_set:
+        position_set.remove(minister_position_entity)
+        minister_position = minister_position_entity
+
     mayor_position_set = set()
     for pos in mayor_position_entities:
         if pos in position_list:
             position_set.remove(pos)
             mayor_position_set.add(pos)
 
-    municipal_position_entities = list(councillor_position_entities)
-    municipal_position_entities.append(deputy_mayor_position_entity)
-    municipal_position_set = set()
-    for pos in municipal_position_entities:
+    deputy_mayor_position = None
+    if deputy_mayor_position_entity in position_set:
+        position_set.remove(deputy_mayor_position_entity)
+        deputy_mayor_position = deputy_mayor_position_entity
+
+    councillor_position_set = set()
+    for pos in councillor_position_entities:
         if pos in position_list:
             position_set.remove(pos)
-            municipal_position_set.add(pos)
-
-    minister_position = None
-    if minister_position_entity in position_set:
-        position_set.remove(minister_position_entity)
-        minister_position = minister_position_entity
+            councillor_position_set.add(pos)
 
     pos_clauses = []
     if minister_position:
@@ -165,7 +137,7 @@ def make_query_url(detail, position_set):
         pos_clauses.append('?p wdt:P279/wdt:P279 %s.' % nmp)
 
     if len(mayor_position_set):
-        city_set = make_city_set_for_mayor(detail)
+        city_set = make_city_set(detail, mayor_position_entities[0])
         for city in city_set:
             mayor = city2mayor.get(city)
             if mayor:
@@ -185,20 +157,17 @@ def make_query_url(detail, position_set):
         filter(%s).""" % (vl, filter_expr)
             pos_clauses.append(bare_clause)
 
-    if len(municipal_position_set):
-        city_set = make_city_set_for_municipal(detail)
-        if len(city_set):
-            vl = ' '.join('wd:' + p for p in sorted(municipal_position_set))
+    if deputy_mayor_position:
+        # constructing city set separately for deputy mayor actually
+        # leads to fewer matches, but even if it works, matching
+        # deputy mayor as councillor is incorrect...
+        bare_clause = format_councillor_bare_clause(detail, (deputy_mayor_position,))
+        if bare_clause:
+            pos_clauses.append(bare_clause)
 
-            # equality should be sufficient but actually doesn't match some labels
-            filter_expr = ' || '.join('strstarts(lcase(?t), "%s")' % c for c in sorted(city_set))
-
-            # city (or village), title (of city - ?l is already taken)
-            bare_clause = """values ?p { %s }
-        ?w p:P39/pq:P642 ?c.
-        ?c rdfs:label ?t.
-        filter(lang(?t) = "cs").
-        filter(%s).""" % (vl, filter_expr)
+    if len(councillor_position_set):
+        bare_clause = format_councillor_bare_clause(detail, councillor_position_set)
+        if bare_clause:
             pos_clauses.append(bare_clause)
 
     if len(position_set):
@@ -207,7 +176,10 @@ def make_query_url(detail, position_set):
 
     l = len(pos_clauses)
     if l == 0:
-        pos_clause = '' # no restriction
+        # no restriction; can happen even when the original position
+        # set is non-empty, and if it causes false positives, we'll
+        # have to revisit...
+        pos_clause = ''
     elif l == 1:
         pos_clause = pos_clauses[0]
     else:
---
+++ python/rulebook.py
@@ -0,0 +1,81 @@
+import re
+
+# ministers are special because their position is a subclass of
+# minister - normally minister of <resort> of Czech Republic
+minister_position_entity = 'Q83307'
+
+# mayors are special because there's so many the name match may
+# produce false positives - so for mayors we also match the city
+mayor_position_entities = ( 'Q30185', 'Q147733' )
+
+deputy_mayor_position_entity = 'Q581817'
+
+# councillors are even commoner than mayors
+councillor_position_entities = ( 'Q708492', 'Q19602879', 'Q4657217' )
+
+# not particularly special but does repeat
+director_position_entity = 'Q1162163'
+
+university_name_rx = re.compile("\\b(?:univerzita|učení)")
+
+university2rector = {
+    'Univerzita Karlova': 'Q12049166',
+    'Univerzita Karlova v Praze': 'Q12049166'
+}
+
+# A rulebook (q.v.) value marking the match as a position that should
+# also match a city. Note that instances of this object can be values
+# of multiple rulebook keys, but each individual instance must be
+# initialized with a non-overlapping position set (technically an
+# iterable, or a single string).
+class CityLevel:
+    def __init__(self, positions):
+        self.positions = positions
+
+    def __call__(self, it):
+        return self.positions
+
+def get_org_name(it):
+    return it['organization'].strip()
+
+def produce_rector(it):
+    org_name = get_org_name(it)
+
+    sought = []
+    rector = university2rector.get(org_name)
+    if rector:
+        sought.append(rector)
+
+    if rector or university_name_rx.search(org_name.lower()):
+        sought.extend([ 'Q212071', 'Q2113250', 'Q723682' ])
+
+    return sought
+
+def produce_director(it):
+    org_name = get_org_name(it)
+
+    if (org_name == 'Kancelář prezidenta republiky'):
+        return 'Q15712674'
+    else:
+        return director_position_entity
+
+councillor_city_level = CityLevel(councillor_position_entities)
+
+# Maps it['workingPosition']['name'], where it is an item of cro
+# detail page JSON attribute 'workingPositions', to position set. The
+# mapping can be a single string, an iterable, or a callable returning
+# a string or an iterable. The callable is called with the it value.
+rule_book = {
+    'člen řídícího orgánu': produce_rector,
+    'vedoucí zaměstnanec 3. stupně řízení': produce_director,
+    'člen vlády': minister_position_entity, # apparently doesn't include deputy ministers (but does include premier)
+    'náměstek pro řízení sekce': 'Q15735113',
+    'starosta': CityLevel(mayor_position_entities),
+    'místostarosta / zástupce starosty': CityLevel(deputy_mayor_position_entity),
+    'člen zastupitelstva': councillor_city_level,
+    'člen Rady': councillor_city_level, # there are other councils, which currently aren't recognized
+    'člen bankovní rady České národní banky': 'Q28598459', # missing the governor
+    'soudce': 'Q16533',
+    'ředitel bezpečnostního sboru': director_position_entity,
+    'vedoucí zastupitelského úřadu': 'Q121998',
+}
