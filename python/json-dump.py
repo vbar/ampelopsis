4080d0dd0748e89ec4b6eb0e94079147617949ea
Vaclav Barta <vbarta@mangrove.cz>
2018-11-14 19:20:23 +0100

    moved json lookup main to own script

---
+++ python/json-dump.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import json
+import sys
+from urllib import parse
+from common import make_connection
+from json_lookup import JsonLookup
+from jump_util import make_position_set, make_query_url
+
+def print_query(qurl):
+    uo = parse.urlparse(qurl)
+    params = parse.parse_qsl(uo.query)
+    for p in params:
+        if p[0] == 'query':
+            print(p[1])
+
+    print("")
+
+def main():
+    with make_connection() as conn:
+        with conn.cursor() as cur:
+            lookup = JsonLookup(cur)
+            for a in sys.argv[1:]:
+                detail = lookup.get_document(a)
+                position_set = make_position_set(detail)
+                qurl = make_query_url(detail, position_set)
+                print_query(qurl)
+                leaf = lookup.get_document(qurl)
+                if leaf:
+                    json.dump(leaf, sys.stdout, ensure_ascii=False)
+                    print("")
+
+                if len(position_set):
+                    qurl = make_query_url(detail, set())
+                    leaf = lookup.get_document(qurl)
+                    if leaf:
+                        print("")
+                        print_query(qurl)
+                        json.dump(leaf, sys.stdout, ensure_ascii=False)
+                        print("")
+
+if __name__ == "__main__":
+    main()
--- python/json_lookup.py
+++ python/json_lookup.py
@@ -3,8 +3,6 @@
 import json
 import re
 import sys
-from urllib import parse
-from common import make_connection
 from cursor_wrapper import CursorWrapper
 from jump_util import make_person_name, make_position_set, make_query_url
 from volume_holder import VolumeHolder
@@ -86,38 +84,3 @@ where url=%s""", (url,))
             return None
 
         return row[0]
-
-def main():
-    def print_query(qurl):
-        uo = parse.urlparse(qurl)
-        params = parse.parse_qsl(uo.query)
-        for p in params:
-            if p[0] == 'query':
-                print(p[1])
-
-        print("")
-
-    with make_connection() as conn:
-        with conn.cursor() as cur:
-            lookup = JsonLookup(cur)
-            for a in sys.argv[1:]:
-                detail = lookup.get_document(a)
-                position_set = make_position_set(detail)
-                qurl = make_query_url(detail, position_set)
-                print_query(qurl)
-                leaf = lookup.get_document(qurl)
-                if leaf:
-                    json.dump(leaf, sys.stdout, ensure_ascii=False)
-                    print("")
-
-                if len(position_set):
-                    qurl = make_query_url(detail, set())
-                    leaf = lookup.get_document(qurl)
-                    if leaf:
-                        print("")
-                        print_query(qurl)
-                        json.dump(leaf, sys.stdout, ensure_ascii=False)
-                        print("")
-
-if __name__ == "__main__":
-    main()
