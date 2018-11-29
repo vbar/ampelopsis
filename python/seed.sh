f6e9a81445bf23ccb5431fc9b85825c87f6ab968
Vaclav Barta <vbarta@mangrove.cz>
2018-11-29 19:33:09 +0100

    moved seed.sh

--- python/seed.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./seed.py 'https://cro.justice.cz/verejnost/api/funkcionari?order=DESC&page=0&pageSize=100&sort=created' "`./jumper.py`"
-
---
+++ seed.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python/seed.py 'https://cro.justice.cz/verejnost/api/funkcionari?order=DESC&page=0&pageSize=100&sort=created' "`python/jumper.py`"
+
