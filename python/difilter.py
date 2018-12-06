b7fdb3f8c99dd021b5ecb4005d99f51404622209
Vaclav Barta <vbarta@mangrove.cz>
2018-12-06 19:22:01 +0100

    added blacklist/whitelist

--- python/difilter.py
+++ python/difilter.py
@@ -3,6 +3,7 @@
 # requires database created with config option jump_links = 2
 
 import json
+import re
 import sys
 from common import make_connection
 from json_lookup import JsonLookup
@@ -24,6 +25,17 @@ class DiFilter(JsonLookup):
         JsonLookup.__init__(self, cur)
         self.mode = mode
         self.verbose = verbose
+        self.black = None
+        self.white = None
+        self.entity_rx = re.compile('/(Q[0-9]+)$')
+
+    def set_blacklist(self, blacklist):
+        self.black = set(blacklist)
+        self.white = None
+
+    def set_whitelist(self, whitelist):
+        self.black = None
+        self.white = set(whitelist)
 
     def run(self):
         self.cur.execute("""select url, id
@@ -73,21 +85,59 @@ order by url""")
             print("")
             print("")
 
+    # doesn't respect filtering in JsonLookup.get_entities - should it?
     def has_answer(self, url):
+        assert (self.black is None) or (self.white is None)
+
         doc = self.get_document(url)
         if not doc:
             return False
 
         bindings = doc['results']['bindings']
-        return len(bindings)
+        if (self.black is None) and (self.white is None):
+            return len(bindings)
+
+        for it in bindings:
+            m = self.entity_rx.search(it['p']['value'])
+            if m:
+                pos = m.group(1)
+                if self.white is None:
+                    if not(pos in self.black):
+                        return True
+                else:
+                    if pos in self.white:
+                        return True
+
+        return False
+
 
 def main():
-    if len(sys.argv) > 4:
+    argv = sys.argv[:]
+
+    entity_rx = re.compile('^Q[1-9][0-9]*$')
+    tail_list = []
+    black_flag = False
+    while len(argv):
+        if entity_rx.match(argv[-1]):
+            tail_list.insert(0, argv.pop())
+        else:
+            break
+
+    if len(argv):
+        a = argv[-1]
+        if a in ('-b', '--black'):
+            argv.pop()
+            black_flag = True
+        elif a in ('-w', '--white'):
+            argv.pop()
+            # black_flag remains false
+
+    if len(argv) > 4:
         raise Exception("too many arguments")
 
     args = []
     verbose = False
-    for a in sys.argv[1:]:
+    for a in argv[1:]:
         if (a == '-v') or (a == '--verbose'):
             verbose = True
         else:
@@ -115,6 +165,12 @@ def main():
         with conn.cursor() as cur:
             mode = UNDERSPECIFIED | OVERSPECIFIED if len(modes) == 2 else modes[0]
             difilter = DiFilter(cur, mode, verbose)
+            if len(tail_list):
+                if black_flag:
+                    difilter.set_blacklist(tail_list)
+                else:
+                    difilter.set_whitelist(tail_list)
+
             difilter.run()
 
 if __name__ == "__main__":
