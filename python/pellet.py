1fff8aedc6b404a326caad526d89f444766d8a98
Vaclav Barta <vbarta@mangrove.cz>
2019-01-01 15:33:06 +0100

    ISO birthdate

--- python/pellet.py
+++ python/pellet.py
@@ -4,7 +4,7 @@ from urllib.parse import urlparse
 # SPARQL query result row subset used for enrichment of JSON results
 # (or any other further processing)
 class Pellet:
-    datetime_rx = re.compile('^([0-9]{4}-[0-9]{2}-[0-9]{2})T00:00:00Z$')
+    datetime_rx = re.compile('^([0-9]{4}-[0-9]{2}-[0-9]{2}T00:00:00)Z$')
 
     def __init__(self, wikidataId, birthDate, aboutLink):
         self.wikidataId = wikidataId
