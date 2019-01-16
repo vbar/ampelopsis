08fdaac4a381a2e1ef9694c99f10dc70719c2127
Vaclav Barta <vbarta@mangrove.cz>
2019-01-16 20:30:28 +0100

    also matching gymnasium

--- python/rulebook_util.py
+++ python/rulebook_util.py
@@ -4,7 +4,7 @@ university_alternatives = "univerzita|učení|škola"
 
 university_name_rx = re.compile("\\b(?:" + university_alternatives + ")\\b")
 
-school_name_rx = re.compile("\\b(?:" + university_alternatives + "|zš)\\b")
+school_name_rx = re.compile("\\b(?:" + university_alternatives + "|zš|gymn[aá][sz]ium)\\b")
 
 def get_org_name(it):
     mixed = it['organization'].strip()
