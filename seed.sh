b21086002dfe8f28a564896111c512806b9f72b5
Vaclav Barta <vbarta@mangrove.cz>
2019-11-04 18:38:25 +0100

    switched order of seed URLs (request to cro.justice.cz tends to fail when first)

--- seed.sh
+++ seed.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-python/seed.py 'https://cro.justice.cz/verejnost/api/funkcionari?order=DESC&page=0&pageSize=100&sort=created' "`python/jumper.py`"
-
+python/seed.py "`python/jumper.py`" 'https://cro.justice.cz/verejnost/api/funkcionari?order=DESC&page=0&pageSize=100&sort=created'
