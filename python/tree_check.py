71cf9b896d35296ce00a4b06ccab2410fe93be6a
Vaclav Barta <vbarta@mangrove.cz>
2019-12-17 21:24:38 +0100

    simplified TreeCheck

--- python/tree_check.py
+++ python/tree_check.py
@@ -1,8 +1,10 @@
 import re
 
 def make_check_rx(words):
+    # matched words are used to look up the resulting entity (meaning,
+    # among other things, that they must match literally)
     alts = "|".join((re.escape(w) for w in words))
-    return re.compile("\\b%s\\b" % alts)
+    return re.compile("\\b(%s)\\b" % alts)
 
 def normalize_value(v):
     if not type(v) is str:
@@ -13,27 +15,27 @@ def normalize_value(v):
 
 class TreeCheck:
     def __init__(self):
-        self.spec = {} # key -> word -> ( regex matching word, entity )
+        self.spec = {} # key -> word -> entity
         self.key2check = None # initialized lazily; key -> regex matching words under key in spec
         self.found = None # initialized before every find; set of entity
 
     def add(self, key, word, entity):
-        word2pair = self.spec.get(key)
-        if word2pair is None:
-            word2pair = {}
-            self.spec[key] = word2pair
+        word2entity = self.spec.get(key)
+        if word2entity is None:
+            word2entity = {}
+            self.spec[key] = word2entity
 
         nword = normalize_value(word)
         if not nword:
             raise Exception("word %s not valid for tree check" % word)
 
-        word2pair[nword] = (re.compile("\\b%s\\b" % re.escape(nword)), entity)
+        word2entity[nword] = entity
 
     def find(self, doc):
         if self.key2check is None:
             self.key2check = {}
-            for key, word2pair in self.spec.items():
-                self.key2check[key] = make_check_rx(sorted(word2pair.keys()))
+            for key, word2entity in self.spec.items():
+                self.key2check[key] = make_check_rx(sorted(word2entity.keys()))
 
         self.found = set()
         self.do_walk(doc)
@@ -45,12 +47,14 @@ class TreeCheck:
                 w = None
                 if k in self.spec.keys():
                     w = normalize_value(v)
-                    check_rx = self.key2check[k]
-                    if w and check_rx.search(w):
-                        word2pair = self.spec[k]
-                        for _, p in word2pair.items():
-                            if p[0].search(w):
-                                self.found.add(p[1])
+                    if w:
+                        check_rx = self.key2check[k]
+                        m = check_rx.search(w)
+                        if m:
+                            fw = m.group(1)
+                            word2entity = self.spec[k]
+                            e = word2entity[fw]
+                            self.found.add(e)
 
                 if w is None:
                     self.do_walk(v)
