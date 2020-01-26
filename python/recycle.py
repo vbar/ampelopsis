b591320b7770da44519f8bec8f80248f34a45c51
Vaclav Barta <vbarta@mangrove.cz>
2020-01-26 08:53:49 +0100

    recycling either CRO, or Wikidata URLs

--- python/recycle.py
+++ python/recycle.py
@@ -9,8 +9,10 @@ from schema_manager import SchemaManager
 
 
 class Recycler(JsonLookup):
-    def __init__(self, old_schema, new_schema, cur):
+    def __init__(self, old_schema, new_schema, cur, primary):
         JsonLookup.__init__(self, cur)
+        self.process = self.process_primary if primary else self.process_secondary
+        self.primary = primary
         self.old_schema = old_schema
         self.new_schema = new_schema
         self.input_count = 0
@@ -19,17 +21,35 @@ class Recycler(JsonLookup):
     def run(self):
         print("searching for recycleable URLs in %s..." % self.old_schema, file=sys.stderr)
 
+        if self.primary:
+            self.run_cycle("^https://cro.justice.cz/verejnost/api/funkcionari\\?order=DESC&page=[0-9]+&pageSize=[0-9]+&sort=created$")
+
+        self.run_cycle("^https://cro.justice.cz/verejnost/api/funkcionari/[a-f0-9-]+$")
+
+    def run_cycle(self, url_mask):
         self.cur.execute("""select url
 from field
 left join download_error on id=url_id
-where url ~ '^https://cro.justice.cz/verejnost/api/funkcionari/[a-f0-9-]+$'
+where url ~ %s
         and checkd is not null and url_id is null
-order by url""")
+order by url""", (url_mask,))
         rows = self.cur.fetchall()
         for row in rows:
             self.process(row[0])
 
-    def process(self, detail_url):
+    def process_primary(self, detail_url):
+        url_id = self.get_url_id(detail_url)
+        if url_id is None:
+            return None
+
+        volume_id = self.get_volume_id(url_id)
+        self.recycle(detail_url, url_id, volume_id)
+
+        self.input_count += 1
+        if not (self.input_count % 1000):
+            print("recycled %d detail URLs..." % self.input_count)
+
+    def process_secondary(self, detail_url):
         detail = self.get_document(detail_url)
         if not detail:
             # select in run should filter out all download errors, but
@@ -99,11 +119,16 @@ where id=%s""", (old_url_id,))
                 shutil.copyfileobj(reader, writer)
 
         with SchemaManager(self.new_schema, self.cur):
-            # generally a new page should go into parse_queue, but
-            # here we know the queries have no further links
+            # generally a new page should go into parse_queue - but
+            # queries have no further links
+            parsed_date = None if self.primary else checkd
             self.cur.execute("""update field
 set checkd=%s, parsed=%s
-where id=%s""", (checkd, checkd, new_url_id))
+where id=%s""", (checkd, parsed_date, new_url_id))
+
+            if parsed_date is None:
+                self.cur.execute("""insert into parse_queue(url_id) values(%s)
+on conflict(url_id) do nothing""", (new_url_id,))
 
         self.output_count += 1
 
@@ -144,12 +169,13 @@ where url_id=%s""", (new_url_id,))
 
 
 def main():
+    primary = common.get_option("recycle_primary", False)
     new_schema = common.schema
     with common.make_connection() as conn:
         with conn.cursor() as cur:
             old_schema = common.get_mandatory_option("old_schema")
             with SchemaManager(old_schema, cur):
-                recycler = Recycler(old_schema, new_schema, cur)
+                recycler = Recycler(old_schema, new_schema, cur, primary)
                 recycler.run()
 
 if __name__ == "__main__":
