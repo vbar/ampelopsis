cd6ef511f53df59d14d3d686bf11177114ceeea4
Vaclav Barta <vbarta@mangrove.cz>
2020-03-21 08:50:07 +0100

    added presentation name

--- python/funnel_parser.py
+++ python/funnel_parser.py
@@ -62,16 +62,22 @@ class FunnelParser:
         # no need to handle relative URLs - we're only interested in
         # the absolute one to Twitter
         context = etree.iterparse(fp, events=('end',), tag=('a'), html=True, recover=True)
+        present_name = None
         for action, elem in context:
             href = elem.get('href')
             if href:
-                m = card_rx.match(href)
-                if m:
-                    town_name = m.group('tname')
-                    print("%s <=> %s" % (hamlet_name, town_name), file=sys.stderr)
-                    self.owner.cur.execute("""insert into vn_identity_hamlet(hamlet_name, town_name)
-values(%s, %s)
-on conflict do nothing""", (hamlet_name, town_name))
+                cls = elem.get('class')
+                if cls and cls.startswith('section-title') and href == '/' and elem.text:
+                    present_name = elem.text.strip()
+                else:
+                    m = card_rx.match(href)
+                    if m:
+                        town_name = m.group('tname')
+                        # page has name heading before social media links
+                        print("%s <=> %s (%s)" % (hamlet_name, town_name, present_name), file=sys.stderr)
+                        self.owner.cur.execute("""insert into vn_identity_hamlet(hamlet_name, town_name, presentation_name)
+values(%s, %s, %s)
+on conflict do nothing""", (hamlet_name, town_name, present_name))
 
             # cleanup
             elem.clear()
--- sql/19-vn_identity_hamlet.sql
+++ sql/19-vn_identity_hamlet.sql
@@ -1,3 +1,4 @@
 create table vn_identity_hamlet(hamlet_name varchar not null,
        town_name varchar not null,
+       presentation_name varchar,
        primary key (hamlet_name, town_name));
