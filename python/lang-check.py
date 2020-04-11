d1cfd450707d0e27b172cd07af778b6afbae2f86
Vaclav Barta <vbarta@mangrove.cz>
2020-04-11 16:37:51 +0200

    randomly sampling language recognition

--- python/lang-check.py
+++ python/lang-check.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
 
+import random
 import sys
-from common import make_connection
+from common import get_option, make_connection
 from lang import init_lang_recog
 from show_case import ShowCase
 from token_util import tokenize
 
+class LangSample:
+    def __init__(self, samples):
+        self.samples = samples
+        self.freq = 1
+
+
 class Processor(ShowCase):
     def __init__(self, cur):
         ShowCase.__init__(self, cur)
+        random.seed()
         self.lang_recog = init_lang_recog()
-        self.lang2freq = {}
+        self.add_threshold = float(get_option("lang_mem_add_threshold", "0.001"))
+        self.replace_threshold = self.add_threshold + float(get_option("lang_mem_replace_threshold", "0.5"))
+        self.lang2sample = {} # str -> LangSample
 
     def load_item(self, et):
         lst = tokenize(et['text'], False)
@@ -18,12 +28,30 @@ class Processor(ShowCase):
         if not lng:
             lng = 'other'
 
-        cnt = self.lang2freq.get(lng, 0)
-        self.lang2freq[lng] = cnt + 1
+        sample = self.lang2sample.get(lng)
+        if not sample:
+            self.lang2sample[lng] = LangSample(samples=[et])
+        else:
+            sample.freq += 1
+            r = random.random()
+            if r < self.add_threshold:
+                sample.samples.append(et)
+            elif r < self.replace_threshold:
+                d = random.randrange(len(sample.samples))
+                del sample.samples[d]
+                sample.samples.append(et)
 
     def dump(self):
-        for lng, frq in sorted(self.lang2freq.items(), key=lambda p: (-1 * p[1], p[0])):
-            print(lng, "\t", frq)
+        items = sorted(self.lang2sample.items(), key=lambda p: (-1 * p[1].freq, p[0]))
+        for lng, sample in items:
+            print(lng, "\t", sample.freq)
+
+        for lng, sample in items:
+            print("")
+            print(lng, "\t", len(sample.samples))
+            for et in sample.samples:
+                print(et['url'])
+                print(et['text'])
 
 
 def main():
