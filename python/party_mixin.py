89b3077a41762d053a7fb10190f0c8f74a1b056c
Vaclav Barta <vbarta@mangrove.cz>
2020-04-12 11:09:02 +0200

    split PinholeBase

---
+++ python/party_mixin.py
@@ -0,0 +1,50 @@
+class PartyMixin: # self.cur must be provided by another inherited class
+    def __init__(self):
+        self.party_map = {} # int party id -> str (short) party name
+        self.person_map = {} # str hamlet name -> str presentation name
+        self.hamlet2town = {} # str -> str
+        self.town2hamlet = {} # str -> str
+        self.hamlet2party = {} # str -> int
+        self.party2color = {} # int party id / 0 -> str color (6 chars, w/o #)
+        self.init_mapping()
+        self.next_shade = 'A'
+
+    def init_mapping(self):
+        self.cur.execute("""select hamlet_name, presentation_name, town_name, vn_record.party_id, party_name, color
+from vn_record
+join vn_identity_hamlet on record_id=vn_record.id
+left join vn_party on vn_party.id=vn_record.party_id
+left join vn_party_name on vn_party.id=vn_party_name.party_id
+order by hamlet_name, town_name""")
+        rows = self.cur.fetchall()
+        for hamlet_name, present_name, town_name, party_id, party_name, color in rows:
+            self.hamlet2town[hamlet_name] = town_name
+            self.town2hamlet[town_name] = hamlet_name
+            self.person_map[hamlet_name] = present_name
+            if party_id:
+                self.hamlet2party[hamlet_name] = party_id
+
+                old_name = self.party_map.get(party_id)
+                if (old_name is None) or (len(old_name) > len(party_name)):
+                    self.party_map[party_id] = party_name
+
+                if color:
+                    self.party2color[party_id] = color
+
+    def introduce_color(self, variant):
+        if type(variant) is str:
+            party_id = self.hamlet2party.get(variant, 0)
+        else:
+            party_id = variant
+
+        color = self.party2color.get(party_id)
+
+        if not color:
+            color = self.next_shade * 6
+            self.next_shade = chr(ord(self.next_shade) + 1)
+            if self.next_shade == 'D': # independents have that
+                self.next_shade = 'A'
+
+            self.party2color[party_id] = color
+
+        return '#' + color
--- python/pinhole_base.py
+++ python/pinhole_base.py
@@ -1,21 +1,15 @@
 import json
 import networkx as nx
+from party_mixin import PartyMixin
 from show_case import ShowCase
 
-class PinholeBase(ShowCase):
+class PinholeBase(ShowCase, PartyMixin):
     def __init__(self, cur, distinguish, deconstructed):
         ShowCase.__init__(self, cur)
+        PartyMixin.__init__(self)
         self.distinguish = distinguish
         self.deconstructed = set() # of int party id
         self.init_deconstructed(deconstructed)
-        self.party_map = {} # int party id -> str (short) party name
-        self.person_map = {} # str hamlet name -> str presentation name
-        self.hamlet2town = {} # str -> str
-        self.town2hamlet = {} # str -> str
-        self.hamlet2party = {} # str -> int
-        self.party2color = {} # int party id / 0 -> str color (6 chars, w/o #)
-        self.init_mapping()
-        self.next_shade = 'A'
         self.pair2node = {} # (str hamlet name / int party id, bool from?) -> int node index
         self.node2variant = {} # int node index -> str hamlet name / int party id
         self.ref_map = {} # (int, int) edge -> int weight
@@ -32,28 +26,6 @@ order by party_id""", (tuple(deco_set),))
         rows = self.cur.fetchall()
         for row in rows:
             self.deconstructed.add(row[0])
-
-    def init_mapping(self):
-        self.cur.execute("""select hamlet_name, presentation_name, town_name, vn_record.party_id, party_name, color
-from vn_record
-join vn_identity_hamlet on record_id=vn_record.id
-left join vn_party on vn_party.id=vn_record.party_id
-left join vn_party_name on vn_party.id=vn_party_name.party_id
-order by hamlet_name, town_name""")
-        rows = self.cur.fetchall()
-        for hamlet_name, present_name, town_name, party_id, party_name, color in rows:
-            self.hamlet2town[hamlet_name] = town_name
-            self.town2hamlet[town_name] = hamlet_name
-            self.person_map[hamlet_name] = present_name
-            if party_id:
-                self.hamlet2party[hamlet_name] = party_id
-
-                old_name = self.party_map.get(party_id)
-                if (old_name is None) or (len(old_name) > len(party_name)):
-                    self.party_map[party_id] = party_name
-
-                if color:
-                    self.party2color[party_id] = color
 
     def dump_standard(self):
         self.lazy_ref_map()
@@ -153,21 +125,3 @@ order by hamlet_name, town_name""")
             self.node2variant[node_idx] = variant
 
         return node_idx
-
-    def introduce_color(self, variant):
-        if type(variant) is str:
-            party_id = self.hamlet2party.get(variant, 0)
-        else:
-            party_id = variant
-
-        color = self.party2color.get(party_id)
-
-        if not color:
-            color = self.next_shade * 6
-            self.next_shade = chr(ord(self.next_shade) + 1)
-            if self.next_shade == 'D': # independents have that
-                self.next_shade = 'A'
-
-            self.party2color[party_id] = color
-
-        return '#' + color
