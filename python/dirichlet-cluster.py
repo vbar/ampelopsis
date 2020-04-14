4e38918749084c8e324c5a3a4e8a98eb9fe0680d
Vaclav Barta <vbarta@mangrove.cz>
2020-04-14 19:54:06 +0200

    basic clustering

---
+++ python/dirichlet-cluster.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import re
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import CountVectorizer
+import sys
+from common import get_option, make_connection
+from lang_wrap import init_lang_recog
+from show_case import ShowCase
+from token_util import tokenize, retokenize
+
+class Processor(ShowCase):
+    def __init__(self, cur, stop_words):
+        ShowCase.__init__(self, cur)
+        self.stop_words = stop_words
+        self.lang_recog = init_lang_recog()
+        self.docs = []
+
+    def load_item(self, et):
+        lst = tokenize(et['text'], False)
+        lng = self.lang_recog.check(lst)
+        if lng == 'cs':
+            long_lst = tokenize(et['text'], True)
+            txt = " ".join(long_lst)
+            self.docs.append(txt)
+
+    def process(self):
+        cv = CountVectorizer(max_df=0.95, min_df=2, tokenizer=retokenize, stop_words=self.stop_words)
+        df = cv.fit_transform(self.docs)
+        words = cv.get_feature_names()
+        lda = LatentDirichletAllocation(n_components=4)
+        lda.fit(df)
+
+        for index, topic in enumerate(lda.components_):
+            print([words[i] for i in topic.argsort()[-15:]])
+
+
+def main():
+    print("loading stop words...", file=sys.stderr)
+    stop_list_file = get_option("stop_list_file", "stoplist.txt")
+    stop_words = []
+    with open(stop_list_file) as f:
+        for ln in f:
+            lst = ln.split()
+            if lst:
+                stop_words.append(lst[0])
+
+    with make_connection() as conn:
+        with conn.cursor() as cur:
+            processor = Processor(cur, stop_words)
+            processor.run()
+            processor.process()
+
+if __name__ == "__main__":
+    main()
---
+++ python/wordlist.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import sys
+from common import make_connection
+from show_case import ShowCase
+from token_util import tokenize
+
+class WordList(ShowCase):
+    def __init__(self, cur):
+        ShowCase.__init__(self, cur)
+        self.doc_count = 0
+        self.word2count = {}
+
+    def load_item(self, et):
+        self.doc_count += 1
+        lst = tokenize(et['text'])
+        bag = set(lst)
+        for w in bag:
+            cnt = self.word2count.get(w, 0)
+            self.word2count[w] = cnt + 1
+
+    def dump(self):
+        print("%d documents" % self.doc_count, file=sys.stderr)
+        if self.doc_count:
+            for w, c in sorted(self.word2count.items(), key=lambda p: (-1 * p[1], p[0])):
+                print(w, "\t", c / self.doc_count)
+
+
+def main():
+    with make_connection() as conn:
+        with conn.cursor() as cur:
+            wl = WordList(cur)
+            wl.run()
+            wl.dump()
+
+
+if __name__ == "__main__":
+    main()
