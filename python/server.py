fb795b0f62ee9496c71e077f151c126cd2cf043c
Vaclav Barta <vbarta@mangrove.cz>
2020-10-27 13:11:05 +0100

    using HTTP 1.1

--- python/server.py
+++ python/server.py
@@ -2,6 +2,7 @@
 
 import gzip
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+from io import BytesIO
 from psycopg2 import pool
 import re
 import shutil
@@ -34,6 +35,8 @@ def get_path_rx():
 
 
 class StorageHandler(BaseHTTPRequestHandler):
+    protocol_version = 'HTTP/1.1'
+
     def do_GET(self):
         if not self.path or (self.path == "/"):
             self._serve_root()
@@ -82,60 +85,77 @@ class StorageHandler(BaseHTTPRequestHandler):
         config = """[root]
 instance=%s
 """ % (inst_name,)
+        body = config.encode("utf-8")
 
         self.send_response(200)
         self.send_header('Content-type', "text/plain; encoding=utf-8")
+        self.send_header('Content-Length', len(body))
         self.end_headers()
-        self.wfile.write(config.encode("utf-8"))
-        # self.close_connection = False
+        self.wfile.write(body)
 
     def _serve(self, cur, url_id, headers_flag):
-        compress_threshold = int(get_option("compress_threshold", "100"))
         bridge = StorageBridge(cur)
         try:
             if not bridge.has_local_data(url_id):
                 self.send_error(404, "File not found")
-                return
-
-            volume_id = bridge.get_volume_id(url_id)
-            if headers_flag:
-                sz = None
-                ct = "text/plain"
-                reader = bridge.open_headers(url_id, volume_id)
             else:
-                sz = bridge.get_body_size(url_id, volume_id)
-                ct = bridge.get_content_type(url_id, volume_id)
-                reader = bridge.open_page(url_id, volume_id)
-
-            if reader is None:
-                # headers are optional (e.g. drive.py doesn't store
-                # them); for bodies, assuming redirect
-                self.send_error(204, "No content")
-                return
-
-            try:
-                compress = (sz is not None) and (sz >= compress_threshold) and ('accept-encoding' in self.headers) and ('gzip' in self.headers['accept-encoding'])
-
-                self.send_response(200)
-                self.send_header('Content-Type', ct)
-
-                if compress:
-                    self.send_header('Content-Encoding', 'gzip')
-                elif sz is not None:
-                    self.send_header('Content-Length', sz)
-
-                self.end_headers()
-
-                if compress:
-                    self._write_compressed(reader)
-                else:
-                    shutil.copyfileobj(reader, self.wfile)
-
-                # self.close_connection = False
-            finally:
-                reader.close()
+                self._do_serve(bridge, url_id, headers_flag)
         finally:
             bridge.close()
+
+    def _do_serve(self, bridge, url_id, headers_flag):
+        volume_id = bridge.get_volume_id(url_id)
+        if headers_flag:
+            sz = bridge.get_headers_size(url_id, volume_id)
+            ct = "text/plain"
+        else:
+            sz = bridge.get_body_size(url_id, volume_id)
+            ct = bridge.get_content_type(url_id, volume_id)
+
+        compress_threshold = int(get_option("compress_threshold", "100"))
+
+        # chunked encoding would be better than not compressing large
+        # files, but it's too complicated, and they probably don't
+        # exist anyway...
+        no_compress_threshold = int(get_option("no_compress_threshold", "100000"))
+
+        reader = None
+        if sz:
+            if headers_flag:
+                reader = bridge.open_headers(url_id, volume_id)
+            else:
+                reader = bridge.open_page(url_id, volume_id)
+
+        if reader is None:
+            # headers are optional (e.g. drive.py doesn't store
+            # them); for bodies, assuming redirect
+            self.send_error(204, "No content")
+            return
+
+        try:
+            assert sz
+            compressed = None
+            if (sz >= compress_threshold) and (sz <= no_compress_threshold) and ('accept-encoding' in self.headers) and ('gzip' in self.headers['accept-encoding']):
+                compressed = self._encode_content(reader)
+
+            self.send_response(200)
+            self.send_header('Content-Type', ct)
+
+            if compressed:
+                self.send_header('Content-Encoding', 'gzip')
+                self.send_header('Content-Length', len(compressed))
+            else:
+                self.send_header('Content-Length', sz)
+
+            self.end_headers()
+
+            if compressed:
+                self.wfile.write(compressed)
+                self.wfile.flush()
+            else:
+                shutil.copyfileobj(reader, self.wfile)
+        finally:
+            reader.close()
 
     def _serve_volume(self, cur, volume_id):
         bridge = VolumeBridge(cur)
@@ -143,14 +163,19 @@ instance=%s
             self.send_error(404, "Archive not found")
             return
 
-        reader = bridge.open_volume(volume_id)
+        sz = bridge.get_volume_size(volume_id)
+        reader = None
+        if sz:
+            reader = bridge.open_volume(volume_id)
+
         if not reader:
             self.send_error(404, "Volume not found")
             return
 
         try:
             self.send_response(200)
-            self.send_header('Content-Type', 'application/octet-stream')
+            self.send_header('Content-Type', 'application/zip')
+            self.send_header('Content-Length', sz)
             self.end_headers()
             shutil.copyfileobj(reader, self.wfile)
         finally:
@@ -182,13 +207,15 @@ instance=%s
         bridge.delete_volume(volume_id)
         self.send_error(204, "No content")
 
-    def _write_compressed(self, reader):
-        compressor = gzip.GzipFile(fileobj=self.wfile, mode='w', compresslevel=5)
+    def _encode_content(self, reader):
+        intermediary = BytesIO()
+        compressor = gzip.GzipFile(fileobj=intermediary, mode='w', compresslevel=5)
         try:
             shutil.copyfileobj(reader, compressor)
         finally:
             compressor.close()
 
+        return intermediary.getvalue()
 
 def main():
     try:
