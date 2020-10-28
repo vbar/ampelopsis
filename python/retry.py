ea55047b105a72f3b82cccd10ccd920b8b868486
Vaclav Barta <vbarta@mangrove.cz>
2020-10-28 19:26:55 +0100

    retry also for localized and/or compressed pages

--- python/retry.py
+++ python/retry.py
@@ -7,19 +7,25 @@ from seed import Seeder
 def main():
     with make_connection() as conn:
         with conn.cursor() as cur:
+            inner_select = """select url_id
+from download_error"""
+
             cur.execute("""update field
 set checkd=null
-where id in (
-        select url_id
-        from download_error
-)""")
+where id in (%s)""" % inner_select)
+
+            cur.execute("""delete from locality
+where url_id in (%s)""" % inner_select)
+            cur.execute("""delete from content
+where url_id in (%s)""" % inner_select)
+
             cur.execute("delete from download_error")
 
             kicker = Kicker(cur)
             kicker.run()
 
             seeder = Seeder(cur)
-            seeder.run()
+            seeder.seed_queue()
 
 if __name__ == "__main__":
     main()
