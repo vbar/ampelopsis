63760297a25234507013bb9abe8c84890ff8a229
Vaclav Barta <vbarta@mangrove.cz>
2020-11-22 15:52:36 +0100

    added datemap samples

--- python/dirichlet-date.py
+++ python/dirichlet-date.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 
+import collections
 import json
 import numpy as np
 import sys
-from common import make_connection
+from common import get_option, make_connection
 from dirichlet_base import DirichletBase
 from stop_util import load_stop_words
+
+SampleOcc = collections.namedtuple('SampleOcc', 'url topic_prob')
 
 class Processor(DirichletBase):
     def __init__(self, cur, stop_words):
         DirichletBase.__init__(self, cur, stop_words)
+        self.cell_sample_size = int(get_option('datemap_sample_size', "3"))
+        if self.cell_sample_size < 1:
+            raise Exception("datemap_sample_size must be positive")
+
         self.url2date = {}
         self.evolution = {} # rounded datetime -> array of float activity (indexed by topic)
+        self.cell2samples = {} # rounded datetime, topic index pair -> array of at most cell_sample_size SampleOcc sorted by float SampleOcc.topic_prob, descending
 
     def load_doc(self, et):
         txt = self.reconstitute(et)
@@ -36,7 +44,25 @@ class Processor(DirichletBase):
                 self.evolution[rdt] = instant
 
             for j in range(topic_count):
-                instant[j] += self.matrix[i][j]
+                tp = self.matrix[i][j]
+                instant[j] += tp
+
+                samples = self.cell2samples.get((rdt, j))
+                if samples is None:
+                    self.cell2samples[(rdt, j)] = [ SampleOcc(url=url, topic_prob=tp) ]
+                else:
+                    sort_needed = True
+                    if len(samples) < self.cell_sample_size:
+                        samples.append(SampleOcc(url=url, topic_prob=tp))
+                    else:
+                        worst_occ = samples[-1]
+                        if worst_occ.topic_prob < tp:
+                            samples[-1] = SampleOcc(url=url, topic_prob=tp)
+                        else:
+                            sort_needed = False # no change
+
+                    if sort_needed:
+                        self.cell2samples[(rdt, j)] = sorted(samples, key=lambda so: -1 * so.topic_prob)
 
         # memory optimization
         self.matrix = None
@@ -46,6 +72,7 @@ class Processor(DirichletBase):
             'rowDesc': self.topics,
             'colDesc': self.get_dates(),
             'table': self.get_table(),
+            'samples': self.get_samples(),
             'maxValue': self.get_max_value()
         }
 
@@ -70,6 +97,20 @@ class Processor(DirichletBase):
             di += 1
 
         return table
+
+    def get_samples(self):
+        matrix = []
+        topic_count = len(self.topics)
+        for dt, instant in sorted(self.evolution.items(), key=lambda p: p[0]):
+            row = []
+            for j in range(topic_count):
+                samples = self.cell2samples.get((dt, j))
+                cell = [ s.url for s in samples ] if samples is not None else []
+                row.append(cell)
+
+            matrix.append(row)
+
+        return matrix
 
     def get_max_value(self):
         mx = 0.0
