70f0676ae3669f5151b593ebf4ca3b943a9591ac
Vaclav Barta <vbarta@mangrove.cz>
2021-01-02 19:24:49 +0100

    added series variance (and order)

--- python/rain-hyper.py
+++ python/rain-hyper.py
@@ -4,6 +4,7 @@
 # filled by running condensate.py
 
 import json
+import numpy as np
 import sys
 from common import get_option, make_connection
 from rain_processor import RainProcessor
@@ -22,8 +23,9 @@ def run(cur):
         processor.run()
 
         data = []
+        variance = []
         for idx in range(hyper_repeat):
-            result = []
+            line = []
             for cluster_count in range(cluster_count_bottom, cluster_count_top + 1, cluster_count_stride):
                 processor.cluster_count = cluster_count
                 processor.value_series = None
@@ -31,12 +33,16 @@ def run(cur):
                 gap = processor.get_distance_gap()
                 print("round %d of %d: %d -> %f" % (idx + 1, hyper_repeat, cluster_count, gap), file=sys.stderr)
 
-                result.append([cluster_count, gap])
+                line.append([cluster_count, gap])
 
-            data.append(result)
+            data.append(line)
+
+            liney = [ it[1] for it in line ]
+            variance.append(np.var(liney))
 
         custom = {
-            'data': data
+            'data': data,
+            'variance': variance
         }
 
         if processor.mindate and processor.maxdate:
