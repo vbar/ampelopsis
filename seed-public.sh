bc417857d06c429e3b1a9945bd0a64e0458f64e3
Vaclav Barta <vbarta@mangrove.cz>
2021-05-29 08:11:49 +0200

    seeding jump links

--- seed-public.sh
+++ seed-public.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-# jumper is obsolete, not updated yet
-python/seed.py 'https://cro.justice.cz/verejnost/api/funkcionari?order=DESC&page=0&pageSize=100&sort=created'
+python/seed.py "`python/jumper.py`" 'https://cro.justice.cz/verejnost/api/funkcionari?order=DESC&page=0&pageSize=100&sort=created'
