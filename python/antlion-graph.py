cc9540e3b50df7a804936afd6e8ffaee85d7b88e
Vaclav Barta <vbarta@mangrove.cz>
2021-09-16 09:00:22 +0200

    optionally summarizing spam per target account

--- python/antlion-graph.py
+++ python/antlion-graph.py
@@ -6,25 +6,24 @@ import sys
 from urllib.parse import urlparse, urlunparse
 from common import get_option, make_connection
 from cursor_wrapper import CursorWrapper
+from show_case import ShowCase
 
 status_rx = re.compile("^/([-\\w]+)/status/")
 
 class Checker(CursorWrapper):
-    def __init__(self, cur, mode):
+    def __init__(self, cur, maw_mode):
         CursorWrapper.__init__(self, cur)
-        if mode == 'maw':
+        if maw_mode:
             self.maw_threshold = int(get_option("maw_threshold", "10"))
             self.account2max = {}
             self.process = self.process_maw
-            self.dump = self.dump_maw
+            self.dump_body = self.dump_maw
         else:
             self.track = []
             self.process = self.process_default
-            self.dump = self.dump_default
+            self.dump_body = self.dump_default
 
-    def run(self, writer):
-        writer.writerow(['url', 'count'])
-
+    def run(self):
         self.cur.execute("""select url, tt.c
 from field
 join (
@@ -36,7 +35,20 @@ order by tt.c desc""")
         for row in rows:
             self.process(row[0], row[1])
 
-        self.dump(writer)
+    def get_maw(self):
+        maw = set()
+        for url, cnt in sorted(self.account2max.items(), key=lambda p: (-1 * p[1], p[0])):
+            if cnt >= self.maw_threshold:
+                maw.add(url)
+            else:
+                return maw
+
+        # won't get here in practice, but for completeness...
+        return maw
+
+    def dump(self, writer):
+        writer.writerow(['url', 'count'])
+        self.dump_body(writer)
 
     def process_default(self, url, cnt):
         self.track.append([url, cnt])
@@ -67,12 +79,90 @@ order by tt.c desc""")
             writer.writerow([url, cnt])
 
 
+class Position:
+    def __init__(self, inside):
+        if inside:
+            self.stand = 0
+            self.fall = 1
+        else:
+            self.stand = 1
+            self.fall = 0
+
+    def increment(self, inside):
+        if inside:
+            self.fall += 1
+        else:
+            self.stand += 1
+
+    def total(self):
+        return self.stand + self.fall
+
+    def survival(self):
+        return self.stand / self.total()
+
+
+class PitChecker(ShowCase):
+    def __init__(self, cur, maw):
+        ShowCase.__init__(self, cur)
+        self.maw = maw
+        self.account2mix = {} # profile URL -> Position
+
+    def dump(self, writer):
+        writer.writerow(['url', 'total', 'spam'])
+
+        for url, pos in sorted(self.account2mix.items(), key=lambda p: (-1 * p[1].total(), p[1].survival(), p[0])):
+            row = [ url, pos.total(), pos.fall ]
+            writer.writerow(row)
+
+    def load_item(self, et):
+        town_url = et.get('url')
+        if town_url:
+            self.cur.execute("""select f2.url
+from field f1
+join redirect on f1.id=from_id
+join field f2 on to_id=f2.id
+where f1.url=%s""", (town_url,))
+            rows = self.cur.fetchall()
+            for row in rows:
+                self.process(town_url, row[0])
+
+    def process(self, source_url, target_url):
+        src_acct_url = self.get_account_url(source_url, False)
+        tgt_acct_url = self.get_account_url(target_url, True)
+        if not src_acct_url or not tgt_acct_url:
+            print("skipping redirect from %s to %s" % (source_url, target_url), file=sys.stderr)
+            return
+
+        inside = tgt_acct_url in self.maw
+        pos = self.account2mix.get(src_acct_url)
+        if pos is None:
+            self.account2mix[src_acct_url] = Position(inside)
+        else:
+            pos.increment(inside)
+
+    def get_account_url(self, url, lowercase):
+        pr = urlparse(url)
+        m = status_rx.match(pr.path)
+        if not m:
+            return None
+
+        town_name = m.group(1)
+        name = town_name.lower() if lowercase else town_name
+        account_pr = (pr.scheme, pr.netloc, name, '', '', '')
+        return urlunparse(account_pr)
+
+
 def main():
-    mode = None
+    maw_mode = False
+    pit_mode = False
     l = len(sys.argv)
     if l == 2:
-        if sys.argv[1] == '--maw':
-            mode = 'maw'
+        raw_mode = sys.argv[1]
+        if raw_mode == '--maw':
+            maw_mode = True
+        elif raw_mode == '--pit':
+            maw_mode = True
+            pit_mode = True
         else:
             raise Exception("invalid argument")
     elif l > 2:
@@ -80,9 +170,16 @@ def main():
 
     with make_connection() as conn:
         with conn.cursor() as cur:
-            checker = Checker(cur, mode)
+            checker = Checker(cur, maw_mode)
             writer = csv.writer(sys.stdout, delimiter=",")
-            checker.run(writer)
+            checker.run()
+
+            if not pit_mode:
+                checker.dump(writer)
+            else:
+                pit_checker = PitChecker(cur, checker.get_maw())
+                pit_checker.run()
+                pit_checker.dump(writer)
 
 
 if __name__ == "__main__":
