e44ee4a8569c5440cd5c8835cc9ebaa024f337f5
Vaclav Barta <vbarta@mangrove.cz>
2021-10-14 15:14:37 +0200

    coloring attachments by submitter

--- python/typelength-graph.py
+++ python/typelength-graph.py
@@ -3,50 +3,66 @@
 import json
 import sys
 from common import get_option, make_connection
-from show_cabinet import ShowCabinet
+from show_room import ShowRoom
 from token_util import tokenize
 
-class Processor(ShowCabinet):
+class Processor(ShowRoom):
     def __init__(self, cur):
-        ShowCabinet.__init__(self, cur)
+        ShowRoom.__init__(self, cur)
         self.type2count = {}
+        self.submitter2count = {}
         self.data = []
 
-    def load_item(self, att):
-        self.extend_date(att)
+    def load_item(self, doc):
+        self.extend_date(doc)
 
-        tp = att['typ']
-        cnt = self.type2count.get(tp, 0)
-        self.type2count[tp] = 1 + cnt
+        submitter = doc.get('predkladatel', '')
+        cnt = self.submitter2count.get(submitter, 0)
+        self.submitter2count[submitter] = 1 + cnt
 
-        txt = att.get('DocumentPlainText')
-        if not txt:
-            length = 0
-        else:
-            lst = tokenize(txt)
-            length = len(lst)
+        attachments = doc.get('prilohy')
+        if isinstance(attachments, list):
+            for att in attachments:
+                tp = att['typ']
+                cnt = self.type2count.get(tp, 0)
+                self.type2count[tp] = 1 + cnt
 
-        if length:
-            raw_url = att.get('DocumentUrl')
-            url = self.get_circuit_url(raw_url) if raw_url else "???"
-            item = [ url, tp, length ]
-            self.data.append(item)
+                txt = att.get('DocumentPlainText')
+                if not txt:
+                    length = 0
+                else:
+                    lst = tokenize(txt)
+                    length = len(lst)
+
+                if length:
+                    raw_url = att.get('DocumentUrl')
+                    url = self.get_circuit_url(raw_url) if raw_url else "???"
+                    item = [ url, tp, length, submitter ]
+                    self.data.append(item)
 
     def dump(self):
         keys = [ p[0] for p in sorted(self.type2count.items(), key=lambda q: (q[1], q[0])) ]
-        indirect = {}
+        indirect_type = {}
         types = []
         for idx, tp in enumerate(keys):
-            indirect[tp] = idx
+            indirect_type[tp] = idx
             types.append(tp)
 
+        submitter_keys = [ p[0] for p in sorted(self.submitter2count.items(), key=lambda q: (q[1], q[0])) ]
+        indirect_submitter = {}
+        submitters = []
+        for idx, submitter in enumerate(submitter_keys):
+            indirect_submitter[submitter] = idx
+            submitters.append(submitter)
+
         data = []
-        for url, tp, length in self.data:
-            item = [ url, indirect[tp], length ]
+        for url, tp, length, submitter in self.data:
+            item = [ url, indirect_type[tp], length, indirect_submitter[submitter] ]
             data.append(item)
 
         custom = {
             'types': types,
+            'submitters': submitters,
             'data': data,
             'dateExtent': self.make_date_extent()
         }
