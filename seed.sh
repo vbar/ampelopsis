bb2e28f4845cf58e6c0b62ed69632bdcba3b8c52
Vaclav Barta <vbarta@mangrove.cz>
2022-01-02 14:58:39 +0100

    added 2013 legislature

--- seed.sh
+++ seed.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python/seed.py www.vlada.cz "`python/baker.py`" https://www.psp.cz/eknih/2017ps/stenprot/zip/index.htm https://www.psp.cz/eknih/2021ps/stenprot/zip/index.htm
+python/seed.py www.vlada.cz "`python/baker.py`" https://www.psp.cz/eknih/2013ps/stenprot/zip/index.htm https://www.psp.cz/eknih/2017ps/stenprot/zip/index.htm https://www.psp.cz/eknih/2021ps/stenprot/zip/index.htm
